416b5768d767e52473fb46ccb31f4315bd867fe8
Yijia Liang <Yijia@Yijias-MBP.attlocal.net>
2021-03-14 08:29:33 -0400

    working

--- payload.js
+++ payload.js
@@ -1,10 +1,74 @@
-console.log("here1");
-let sniper = document.getElementById("Sniper");
+window.addEventListener ("load", myMain, false);
 
-sniper.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true });
-    // send the page title as a chrome message
-    chrome.runtime.sendMessage(tab.title, function(response) {
-        console.log(tab.title);
-    });
-});
+var currentURL = location.href;
+
+window.onclick=function(){
+  if(currentURL!=location.href){
+    console.log("tracked change");
+    if (document.getElementById("moment-detailed-serialNumber") != null) {
+        myMain();
+    }
+    currentURL = location.href
+  }
+}
+
+function myMain () {
+    console.log("initializing");
+
+    // Grab New Listing Array with struct Listing
+    var scrapedListings = GetListings("moment-detailed-serialNumber");
+
+    // sort listings in ascending order by price
+    scrapedListings.sort((x,y) => (x.price > y.price ? 1 : -1));
+
+    // make new options set with the sorted list
+    var newOptions = [];
+    newOptions.push("<option value>" + "-- Select a Serial Number --" + "</option>");
+    for (i = 0; i < scrapedListings.length; i++) {
+        if (scrapedListings[i] != null) {
+            scrapedListings[i].text = rewriteText(scrapedListings[i].serial, scrapedListings[i].price);
+            newOptions.push("<option value='" + scrapedListings[i].serial + "'>" + scrapedListings[i].text + "</option>");
+        }
+    }
+
+    // replace options in DOM
+    if (document.getElementById("moment-detailed-serialNumber") != null) {
+        document.getElementById("moment-detailed-serialNumber").innerHTML = newOptions.join();
+    }
+    // scrapedListings.forEach( function(entry) {
+    //     console.log(entry);
+    // });
+}
+
+function Listing(serial, price, text) {
+    this.serial = serial;
+    this.price = price;
+    this.text = text;
+}
+
+function rewriteText(serial, price) {
+    var listing = "#" + serial + " - $" + price;
+    return listing;
+}
+
+function getPrice(str) {
+    var price = str.split('$')[1];
+    var price2 = price.replace(",", "");
+    var cleanedPrice = price2.split('.')[0];
+    return Number(cleanedPrice);
+}
+
+function GetListings(containerID) {
+    var listingsArray = new Array();
+    var listings = document.getElementById(containerID);
+    if (listings != null) {
+        for (i = 0; i < listings.options.length; i++) {
+            if (listings.options[i].value != "") {
+                var price = getPrice(listings.options[i].text);
+                var serial = listings.options[i].value;
+                listingsArray[i] = new Listing(serial, price, "");
+            }
+        }
+    }
+    return listingsArray;
+}
