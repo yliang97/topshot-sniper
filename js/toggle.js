88a30c94456453524e6913e76854867e5340f28c
Yijia Liang <Yijia@Yijias-MBP.attlocal.net>
2021-03-15 10:00:23 -0400

    Updated working version

--- js/toggle.js
+++ js/toggle.js
@@ -12,32 +12,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // only toggle on click
 document.getElementById("toggle").addEventListener("click", function (){
-    console.log("toggled");
+    console.log("toggled - globalStatus is " + globalStatus);
     toggle("toggle");
 }); 
 
 // run this if functon off of chrome storage
 
 function toggle(id_name) {
-//   if (document.getElementById(id_name).value == "False") {
-//     document.getElementById(id_name).value = "True"
-//     document.getElementById(id_name).checked = true;
-//   } else if (document.getElementById(id_name).value == "True") {
-//     document.getElementById(id_name).value = "False";
-//     document.getElementById(id_name).checked = false;
-//   }
-  if (!globalStatus) {
-      document.getElementById(id_name).checked = true;
-  } else if (globalStatus) {
-      document.getElementById(id_name).checked = false;
-  }
+    if (!globalStatus) {
+        document.getElementById(id_name).checked = true;
+        globalStatus = true;
+    } else if (globalStatus) {
+        document.getElementById(id_name).checked = false;
+        globalStatus = false;
+    }
 
   // set stored selection post toggle
-  var switchState = document.getElementById(id_name).checked;
+    var switchState = document.getElementById(id_name).checked;
     chrome.storage.sync.set({
         'value' : switchState
-    }, function () {
-        console.log("Switch Saved as " + switchState);
+        }, function () {
+            console.log("Switch Saved as " + switchState);
     });
 }
 
